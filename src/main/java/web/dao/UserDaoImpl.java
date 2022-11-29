d22111c37d06f0622fd643be12c195c48f81b079
Gleb Kondratenko <nekand@gmail.com>
2022-11-29 10:10:24 +0300

    Add files via upload

--- src/main/java/web/dao/UserDaoImpl.java
+++ src/main/java/web/dao/UserDaoImpl.java
@@ -1,5 +1,7 @@
 package web.dao;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import web.model.User;
@@ -7,28 +9,22 @@ import web.model.User;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
-
 @Repository
-
 public class UserDaoImpl implements UserDao {
     @PersistenceContext
     private EntityManager manager;
-    @Transactional
     @Override
     public List<User> getAllUsers() {
         return manager.createQuery("select us from User us").getResultList();
     }
-    @Transactional
     @Override
     public User getUserById(int id) {
         return manager.find(User.class,id);
     }
-    @Transactional
     @Override
     public void saveUser(User user) {
         manager.persist(user);
     }
-    @Transactional
     @Override
     public void updateUser(int id, User updatedUser) {
         User user = manager.find(User.class,id);
@@ -37,7 +33,6 @@ public class UserDaoImpl implements UserDao {
         user.setAge(updatedUser.getAge());
         user.setEmail(updatedUser.getEmail());
     }
-    @Transactional
     @Override
     public void deleteUser(int id) {
         manager.remove(getUserById(id));
--- src/main/java/web/service/UserServiceImpl.java
+++ src/main/java/web/service/UserServiceImpl.java
@@ -2,6 +2,7 @@ package web.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import web.dao.UserDao;
 import web.model.User;
 
@@ -9,32 +10,32 @@ import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
-
     private final UserDao userDao;
 
     public UserServiceImpl(UserDao dao) {
         this.userDao = dao;
     }
+    @Transactional
     @Override
     public List<User> getAllUsers() {
         return userDao.getAllUsers();
     }
-
+    @Transactional
     @Override
     public User getUserById(int id) {
         return userDao.getUserById(id);
     }
-
+    @Transactional
     @Override
     public void saveUser(User user) {
         userDao.saveUser(user);
     }
-
+    @Transactional
     @Override
     public void updateUser(int id, User updatedUser) {
         userDao.updateUser(id,updatedUser);
     }
-
+    @Transactional
     @Override
     public void deleteUser(int id) {
         userDao.deleteUser(id);
