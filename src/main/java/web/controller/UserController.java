b28e0fb06d0d90e06420fc0c7ed96252c7d28aed
Gleb Kondratenko <nekand@gmail.com>
2022-11-30 23:48:07 +0300

    Add files via upload

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import web.model.User;
+import web.service.UserService;
 import web.service.UserServiceImpl;
 
 import javax.validation.Valid;
@@ -14,10 +15,10 @@ import java.util.List;
 @Controller
 @RequestMapping("/users")
 public class UserController {
-    private final UserServiceImpl userService;
+    private final UserService userService;
 
-    public UserController(UserServiceImpl service) {
-        this.userService = service;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
     @GetMapping("/{id}")
