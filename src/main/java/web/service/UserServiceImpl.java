2f5f191edbbdeb8a628c69d634a087f48eafde22
Gleb Kondratenko <nekand@gmail.com>
2022-11-30 23:42:47 +0300

    Add files via upload

--- src/main/java/web/service/UserServiceImpl.java
+++ src/main/java/web/service/UserServiceImpl.java
@@ -15,22 +15,27 @@ public class UserServiceImpl implements UserService {
     public UserServiceImpl(UserDao dao) {
         this.userDao = dao;
     }
+    @Transactional(readOnly = true)
     @Override
     public List<User> getAllUsers() {
         return userDao.getAllUsers();
     }
+    @Transactional(readOnly = true)
     @Override
     public User getUserById(int id) {
         return userDao.getUserById(id);
     }
+    @Transactional
     @Override
     public void saveUser(User user) {
         userDao.saveUser(user);
     }
+    @Transactional
     @Override
     public void updateUser(int id, User updatedUser) {
         userDao.updateUser(id,updatedUser);
     }
+    @Transactional
     @Override
     public void deleteUser(int id) {
         userDao.deleteUser(id);
